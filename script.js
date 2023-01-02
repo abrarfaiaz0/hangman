7b46596ff11a719216acac1c02166d0a52556970
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-01-02 07:31:42 +0600

    added event listeners on script.js

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ const start = document.querySelector("#start-game");
 const help = document.querySelector("#help");
 const contribute = document.querySelector("#contribute");
 
+
+start.addEventListener("click", startGame);
+help.addEventListener("click", getHelp);
+contribute.addEventListener("click", gotoGithub)
+
+
 function startGame()
 {
-    window.location = "gamepage.html"
+    window.location.assign("gamepage.html");
 }
 
-start.onclick = startGame;
+
+function getHelp()
+{
+    window.location.assign("help.html");
+}
+
+
+function gotoGithub()
+{
+    window.open("https://github.com/abrarfaiaz0/hangman");
+}
+
+

62024a6840ef981fb121302ae9765563c366da37
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-01-04 10:13:54 +0600

    finalized gamepage.html

---
+++ logic.js
@@ -0,0 +1,50 @@
+
+const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+const word = "REDMAN";
+let guessedRight = [];
+
+var guess = "______";
+
+
+var g = document.querySelector(".guess"); 
+g.innerHTML = guess;
+
+function countLetterPressed(e){
+    // console.log(e.currentTarget.pressedLetter);
+    checkLetter(e.currentTarget.pressedLetter);
+}
+
+function checkLetter(e){
+    for(var i=0; i<word.length;i++) {
+        if (word[i] === e) 
+        {
+            guess = guess.substring(0,i)+e+guess.substring(i+1,word.length);
+            g.innerHTML = guess; 
+            console.log(word.length);
+        }
+    }
+    
+}
+
+
+
+const letter = document.querySelector(".letters");
+alphabet.forEach(element => {
+    // console.log(element);
+    const newDiv = document.createElement("div");
+    const newDiv2 = document.createElement("div");
+    newDiv2.innerHTML = element;
+    newDiv.appendChild(newDiv2);
+    newDiv.classList.add("outer");
+    newDiv2.classList.add("inner");
+    newDiv2.classList.add(element);
+    newDiv.addEventListener("click",countLetterPressed);
+    newDiv.pressedLetter=element;
+    letter.appendChild(newDiv);
+});
+
+
+
+let chances, chances_left;
+
